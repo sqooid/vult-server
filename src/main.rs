23ec18067aa994f17397d0dac3de482cad3c91d2
Lucas Liu <lucasliu7@hotmail.com>
2022-06-03 21:20:17 +1000

    Slightly improved arg logic

--- src/main.rs
+++ src/main.rs
@@ -8,28 +8,38 @@ extern crate rocket;
 use std::env;
 
 use api::server::prepare_server;
+use config::parse_config::read_config;
 
 static HELP_TEXT: &str = r#"
 usage: vult-server [COMMAND]
 
     run - Run the server
     help - Show help text
+    version - Show the current version
 "#;
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = config::parse_config::read_config("config.toml")?;
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        println!("{}", HELP_TEXT);
+        println!("{HELP_TEXT}");
         return Err("Invalid arguments".into());
     }
 
     let command = &args[1];
-    if command == "run" {
+    let config = read_config("config.toml")?;
+
+    // Info commands
+    if command == "help" {
+    } else if command == "version" {
+        println!("version 1.0");
+    } else if command == "run" {
         prepare_server(&config)?;
         let _rocket = rocket::build().mount("/", routes![]).launch().await?;
+    } else {
+        println!("{HELP_TEXT}");
+        return Err("Invalid arguments".into());
     }
 
     Ok(())
